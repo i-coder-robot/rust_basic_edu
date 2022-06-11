07e389d13899b8b905852dde93ea851a8c290f39
i-coder-robot <coder2998@gmail.com>
2022-06-11 09:00:32 +0800

    boolandchar07

---
+++ boolandchar07/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    //使用bool关键字声明一个布尔值，取值，true,false
+    let checked:bool = true;
+    println!("checked {}",checked);
+    //字符： 是字符串的基本组成，也就是单个字符或字，UTF-8作为底层编码，包含数字，字母，Unicode和其他特殊字符。
+    let c='R';
+    println!("c {}",c);
+}
