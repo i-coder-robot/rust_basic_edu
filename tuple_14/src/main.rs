2ba3764c1f5840355997a793d15ccb040d8d7f57
i-coder-robot <coder2998@gmail.com>
2022-06-12 10:32:14 +0800

    14

---
+++ tuple_14/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+   /*
+        let tuple变量名称:(数据类型1，数据类型2,...) = (数据1,数据2,...)
+         let tuple变量名称 = (数据1,数据2,...)
+   */
+    let t = ("Go语言极简一本通","掌握Go语言语法，并且可以完成单体服务应用。");
+    println!("{:?}",t);
+    //元组变量.索引数字
+    println!("{}",t.0);
+    println!("{}",t.1);
+    show_tuple(t);
+    println!("{:?}",t);
+
+    let(book,target) = t;
+    println!("{}",book);
+    println!("{}",target);
+
+}
+fn show_tuple(t:(&str,&str)){
+    println!("{:?}",t);
+}
